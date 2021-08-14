460ab55de4b16baae89bcd632219b8216360d589
jowonjang <88566315+jowonjang@users.noreply.github.com>
2021-08-15 00:52:56 +0900

    되라

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,8 @@ function getClock() {
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerText = `현재 시간은 ${hours}:${minutes}:${seconds}`;
+    TEXT = "현재 시간은 : "
+    clock.innerText = `${TEXT}${hours}:${minutes}:${seconds}`;
 }
 
 getClock()
